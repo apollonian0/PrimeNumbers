943ba90b3a386b596c2e98e7d807cd428c795692
Ben Howenstein <b.howenstein@gmail.com>
2020-06-25 17:35:20 -0500

    Solution

--- PrimeNumbers/PrimeNumbers.cpp
+++ PrimeNumbers/PrimeNumbers.cpp
@@ -1,20 +1,118 @@
-// PrimeNumbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Copyright 2020 Ben Howenstein. All rights reserved.
 //
 
 #include <iostream>
+#include <chrono>
+#include <array>
+
+
+constexpr auto MaxPrime = 100000;
+
+int FoundPrimes[MaxPrime];
+
+int CountPrimesSlow(const int n)
+{
+	if (n <= 0)
+	{
+		return 0;
+	}
+
+    auto numComparisons = 0;
+
+    //std::array<int, MaxPrime> foundPrimes;
+    //int foundPrimes[MaxPrime] = {};
+    int* foundPrimes = FoundPrimes;
+    auto numFoundPrimes = 0;
+
+    auto i = 0, j = 0;
+
+    for (i = 2; i < n; ++i)
+    {
+        for (j = 2; j < i; ++j)
+        {
+            ++numComparisons;
+            if (i % j == 0)
+            {
+                break;
+            }
+        }
+
+        if (i == j)
+        {
+            foundPrimes[numFoundPrimes++] = i;
+        }
+    }
+
+    std::cout << "(comparisons: " << numComparisons << ") ";
+
+    return numFoundPrimes + 1;
+}
+
+int CountPrimesFast(const int n)
+{
+    if (n <= 0)
+    {
+        return 0;
+    }
+
+    auto numComparisons = 0;
+
+	//std::array<int, MaxPrime> foundPrimes = { 2 };
+    //int foundPrimes[MaxPrime] = { 2 };
+    int* foundPrimes = FoundPrimes;
+    foundPrimes[0] = 2;
+    auto numFoundPrimes = 1;
+
+    for (auto testValue = 2; testValue < n; ++testValue)
+    {
+		auto primeIndex = 0;
+		for (; primeIndex < numFoundPrimes; ++primeIndex)
+		{
+            ++numComparisons;
+			if ( testValue % foundPrimes[primeIndex] == 0)
+			{
+				break;
+			}
+		}
+		if (primeIndex == numFoundPrimes)
+		{
+			foundPrimes[numFoundPrimes++] = testValue;
+		}
+    }
+
+    std::cout << "(comparisons:  " << numComparisons << ") ";
+
+    return numFoundPrimes + 1;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    auto runTest = [](const int n)
+    {
+        {
+            const auto startTime = std::chrono::high_resolution_clock::now();
+            const auto numPrimes = CountPrimesSlow(n);
+            const auto endTime = std::chrono::high_resolution_clock::now();
+            const auto timeTaken = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+            std::cout << "(time: " << timeTaken << ") There are " << numPrimes << " primes less than or equal to " << n << "\n";
+        }
+        {
+            const auto startTime = std::chrono::high_resolution_clock::now();
+            const auto numPrimes = CountPrimesFast(n);
+            const auto endTime = std::chrono::high_resolution_clock::now();
+            const auto timeTaken = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+            std::cout << "(time: " << timeTaken << ") There are " << numPrimes << " primes less than or equal to " << n << "\n";
+        }
+    };
+
+    runTest(100);
+    std::cout << "\n\n";
+    runTest(1000);
+    std::cout << "\n\n";
+    runTest(10000);
+    std::cout << "\n\n";
+	runTest(100000);
+	std::cout << "\n\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
